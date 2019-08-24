77c819bb106adba3c759e5061c5d62ac3938137c
Max Pronko <max.pronko@gmail.com>
2019-08-24 21:16:24 +0300

    Added empty Config class

---
+++ gateway/Gateway/Config.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Copyright © Pronko Consulting (https://www.pronkoconsulting.com)
+ * See LICENSE for the license details.
+ */
+declare(strict_types=1);
+
+namespace Pronko\LiqPayGateway\Gateway;
+
+/**
+ * Class Config
+ */
+class Config
+{
+    /**
+     * @return string
+     */
+    public function getPrivateKey()
+    {
+        return '';
+    }
+}
--- gateway/Gateway/Request/Builder/SignatureBuilder.php
+++ gateway/Gateway/Request/Builder/SignatureBuilder.php
@@ -9,6 +9,7 @@ namespace Pronko\LiqPayGateway\Gateway\Request\Builder;
 
 use Magento\Framework\Serialize\SerializerInterface;
 use Magento\Payment\Gateway\Request\BuilderInterface;
+use Pronko\LiqPayGateway\Gateway\Config;
 use Pronko\LiqPayGateway\Gateway\Request\Encoder;
 
 /**
@@ -32,19 +33,27 @@ class SignatureBuilder implements BuilderInterface
     private $serializer;
 
     /**
+     * @var Config
+     */
+    private $config;
+
+    /**
      * SignatureBuilder constructor.
      * @param Encoder $encoder
      * @param BuilderInterface $builder
      * @param SerializerInterface $serializer
+     * @param Config $config
      */
     public function __construct(
         Encoder $encoder,
         BuilderInterface $builder,
-        SerializerInterface $serializer
+        SerializerInterface $serializer,
+        Config $config
     ) {
         $this->encoder = $encoder;
         $this->builder = $builder;
         $this->serializer = $serializer;
+        $this->config = $config;
     }
 
     /**
@@ -57,8 +66,7 @@ class SignatureBuilder implements BuilderInterface
 
         $data = $this->encoder->encode($this->serializer->serialize($requestParams));
 
-        //@TODO implement ConfigInterface
-        $privateKey = '';
+        $privateKey = $this->config->getPrivateKey();
 
         $signature = $this->encoder->encode(sha1($privateKey . $data . $privateKey, 1));
 
