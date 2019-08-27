9b14acb80b1ca953d0014926ed08a881a3a83a7d
Max Pronko <max.pronko@gmail.com>
2019-08-27 05:35:27 +0300

    Added Config class implementation

--- gateway/Gateway/Config.php
+++ gateway/Gateway/Config.php
@@ -7,24 +7,75 @@ declare(strict_types=1);
 
 namespace Pronko\LiqPayGateway\Gateway;
 
+use Magento\Payment\Gateway\ConfigInterface;
+use Magento\Tests\NamingConvention\true\bool;
+use Pronko\LiqPayAdmin\Api\Data\EnvironmentStatusInterface;
+
 /**
  * Class Config
  */
 class Config
 {
-    /**
-     * @return string
+    /**#@+
+     * Configuration keys
      */
-    public function getPrivateKey()
+    const PRODUCTION_PUBLIC_KEY = 'production_public_key';
+    const PRODUCTION_PRIVATE_KEY = 'production_private_key';
+    const SANDBOX_PUBLIC_KEY = 'sandbox_public_key';
+    const SANDBOX_PRIVATE_KEY = 'sandbox_private_key';
+    /**#@-*/
+
+    /**
+     * @var ConfigInterface
+     */
+    private $config;
+
+    /**
+     * Config constructor.
+     * @param ConfigInterface $config
+     */
+    public function __construct(ConfigInterface $config)
     {
-        return '';
+        $this->config = $config;
     }
 
     /**
+     * @param int|null $storeId
      * @return string
      */
-    public function getPublicKey()
+    public function getPrivateKey($storeId = null): string
     {
-        return '';
+        return (string) $this->config->getValue(
+            $this->isSandbox($storeId) ?
+                self::SANDBOX_PRIVATE_KEY :
+                self::PRODUCTION_PRIVATE_KEY,
+            $storeId
+        );
+    }
+
+    /**
+     * @param int|null $storeId
+     * @return string
+     */
+    public function getPublicKey($storeId = null): string
+    {
+        return (string) $this->config->getValue(
+            $this->isSandbox($storeId) ?
+                self::SANDBOX_PUBLIC_KEY :
+                self::PRODUCTION_PUBLIC_KEY,
+            $storeId
+        );
+    }
+
+    /**
+     * @param int|null $storeId
+     * @return bool
+     */
+    public function isSandbox($storeId = null): bool
+    {
+        return (bool) $this->config->getValue(
+            EnvironmentStatusInterface::SANDBOX,
+            $storeId
+        );
     }
 }
