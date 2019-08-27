91f11439dab1a00aa519369f72bb1d0523da12dc
Max Pronko <max.pronko@gmail.com>
2019-08-27 05:39:08 +0300

    Added integration test empty class

---
+++ gateway/Test/Integration/Gateway/PayRequestTest.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Copyright © Pronko Consulting (https://www.pronkoconsulting.com)
+ * See LICENSE for the license details.
+ */
+declare(strict_types=1);
+
+namespace Pronko\LiqPayGateway\Test\Integration\Gateway;
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Class PayRequestTest
+ */
+class PayRequestTest extends TestCase
+{
+    public function testPayRequest()
+    {
+        //@TODO implement test
+    }
+}
